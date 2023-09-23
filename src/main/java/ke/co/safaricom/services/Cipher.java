498f44222194867efd3f8a136c941739a019c362
Julius Ojakaa <ojayallday@gmail.com>
2023-09-23 21:11:59 +0300

    Encrypt method basic/ without validations

--- src/main/java/ke/co/safaricom/services/Cipher.java
+++ src/main/java/ke/co/safaricom/services/Cipher.java
@@ -3,11 +3,20 @@ package ke.co.safaricom.services;
 public class Cipher {
     private String message;
     private int key;
-
+    public String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public Cipher() {
     }
     public String encrypt(){
-        return "encrypt";
+        char[] message= this.getMessage().toCharArray();
+        String output = "";
+        for (int i=0; i<message.length; i++ ){
+            char c = message[i];
+            int charPosition = this.alphabets.indexOf(c);
+            int newPosition = charPosition + this.getKey();
+            char newChar = this.alphabets.charAt(newPosition);
+            output = output + newChar;
+        }
+        return output;
     }
     public String decrypt(){
         return "decrypt";
--- src/test/java/ke/co/safaricom/services/CipherTest.java
+++ src/test/java/ke/co/safaricom/services/CipherTest.java
@@ -1,4 +1,11 @@
 package ke.co.safaricom.services;
 
+import org.junit.jupiter.api.Test;
+
 public class CipherTest {
+        @Test
+        public  void sample(){
+            String msg="hello";
+
+        }
 }
