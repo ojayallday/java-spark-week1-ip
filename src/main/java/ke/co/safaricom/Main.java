b72054c42aeb7194079b0d699e2a59f0eace901a
Julius Ojakaa <ojayallday@gmail.com>
2023-09-23 16:51:19 +0300

    modified front end

--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,30 +1,48 @@
 package ke.co.safaricom;
 
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String message = "";
-        int key = 0;
-        int process = 1;
-
+        Scanner scanner= new Scanner(System.in);
+        //message
+        while (true) {
+            System.out.print("Enter your message: ");
+            String message = scanner.next();
+            //process
+            System.out.print("Choose your process e/d: ");
+            String process = scanner.next();
+            //key
+            System.out.print("Select your key: ");
+            int key = scanner.nextInt();
+            //Next step
+            System.out.print("Do you want to continue? c/q: ");
+            String nextStep = scanner.next();
+            if(!nextStep.startsWith("c")){
+                System.out.print("Thank you and goodbye!!");
+                break;
+            }
+        }
+        /*
         Cipher cipher = new Cipher();
         cipher.setMessage(message);
         cipher.setKey(key);
         if (process == 1){
-            cipher.encrypt(plainText: "" );
+            cipher.encrypt(plainText "" );
         } else if (process==2) {
             cipher.decrypt(encryptedText "");
         }else {
             System.out.println("Invalid process");
         }
-
+        */
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        /*for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
-        }
+        }*/
     }
 }
